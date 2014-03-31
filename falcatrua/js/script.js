0059871a2b88576de4c03d9b9665dcf43f605d1a
guiguetz <guilhermebrunoaff@yahoo.com.br>
2014-03-31 02:51:07 -0300

    Falcatrua: The Game
    v0.0002

--- falcatrua/js/script.js
+++ falcatrua/js/script.js
@@ -1,26 +1,37 @@
 /* Write JavaScript here */
 var pontos = 0;
 var clique = 1;
-var custo = 0;
+var custo = 10;
+var timed = 0;
 
 function manual() {
   pontos += clique;
   refresh();
 }
 
+function auto() {
+  pontos += timed;
+  refresh();
+}
+
+setInterval(refresh(), 1000);
+
+function notManual() {
+  timed+=1;
+}
+
 function upgrade() {
-  preco();
   if (pontos>=custo) {
     clique+=1;
-    pontos-=custo;
+    pontos-=custo; 
+    custo=10*Math.pow(1.15,clique);
   }
   refresh();
 }
 
-function preco() {
-  custo = 10*Math.pow(1.15,clique);
-}
-
 function refresh() {
-    document.getElementById("mostrador").innerHTML = (pontos.toFixed(2) + " Falcatruas");
+  document.getElementById("QtFPC").innerHTML = ("+" + clique + " FP");
+  document.getElementById("custo1").innerHTML = (custo.toFixed(2) + " FP");
+  document.getElementById("QtFPS").innerHTML = ("+" + timed + " FP/s");
+  document.getElementById("mostrador").innerHTML = (pontos.toFixed(2) + " Falcatruas");
 }
