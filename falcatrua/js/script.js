3ec235bbd68df447b3983b8957a470cbc8147f79
guiguetz <guilhermebrunoaff@yahoo.com.br>
2014-04-10 23:35:45 -0300

    hhhhhhhhh

--- falcatrua/js/script.js
+++ falcatrua/js/script.js
@@ -6,42 +6,55 @@ var timed = 0;
 var tier1 = 0;
 var tier2 = 0;
 
+<<<<<<< HEAD
+setInterval(refresh, 1000);
+=======
+setInterval(refresh(), 1000);
+>>>>>>> d862d46bbb4e600d33e4a572dac3387a06a613fc
 
 function manual() {
   pontos += clique;
   refresh();
 }
 
+<<<<<<< HEAD
+=======
 function auto() {
   pontos += timed;
   refresh();
 }
-setInterval(refresh, 1000);
 
+>>>>>>> d862d46bbb4e600d33e4a572dac3387a06a613fc
 function notManual1() {
-  timed+=1;
+  timed+=0.01;
   tier1+=1;
+  document.getElementById("Tier1").innerHTML = (tier1);
+  refresh();
 }
 
 function notManual2() {
   timed+=1;
   tier2+=1;
+  document.getElementById("Tier2").innerHTML = (tier2);
+  refresh();
 }
 
 function upgrade(num) {
   if (pontos>=custo) {
     clique+=num;
     pontos-=custo; 
-    custo[num]=10*Math.pow(1.15,(num));
+    custo=10*Math.pow(1.15,(num));
+	document.getElementById("QtFPC").innerHTML = (clique);
+	document.getElementById("custo").innerHTML = (custo.toFixed(2));
   }
   refresh();
 }
 
 function refresh() {
-  auto();
-  document.getElementById("QtFPC").innerHTML = ("+" + clique + " FP");
-  document.getElementById("custo").innerHTML = (custo.toFixed(2) + " FP");
-  document.getElementById("Tier1").innerHTML = (tier1);
-  document.getElementById("Tier2").innerHTML = (tier2);
   document.getElementById("mostrador").innerHTML = (pontos.toFixed(2));
+<<<<<<< HEAD
+  pontos += timed;
 }
+=======
+}
+>>>>>>> d862d46bbb4e600d33e4a572dac3387a06a613fc
