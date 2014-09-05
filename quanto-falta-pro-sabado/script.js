cd5dfba97d316dca0c7d69f4a04802300a6ee450
Guilherme Bruno <guilhermebrunoaff@yahoo.com.br>
2014-09-05 10:24:11 -0300

    Quanto falta pro sábado?
    Commit inicial

---
+++ quanto-falta-pro-sabado/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){ 
+  action();
+});
+
+function action() {
+  var div = $(".count2");
+  var date = new Date();
+  
+  var dia = date.getDay();
+  var hora = date.getHours();
+  var minuto = date.getMinutes();
+  var segundo = date.getSeconds();
+  
+  var newDia = addZero(5-dia);
+  var newHora = addZero(24-hora);
+  var newMinuto = addZero(60-minuto);
+  var newSegundo = addZero(60-segundo);
+
+  div.empty();
+
+  if (dia == 7) {
+    newDia = addZero(5);
+  }
+
+  if (dia == 6) {
+    div.append("Já é sábado!");
+  } else {
+
+   div.append(
+    "Faltam "+ 
+    checkVal(newDia,'dia')+""+
+    checkVal(newHora,'hora')+", "+
+    checkVal(newMinuto,'minuto')+" e "+
+    checkVal(newSegundo,'segundo')+
+    " para o sábado!"
+    );
+ }
+  setTimeout(function(){action()}, 1000);
+}
+
+function checkVal(i,label) {
+  var j = parseInt(i);
+  if (equalsZero(j)) {
+   i = '';
+ } else {
+   i = i+" "+label+isPlural(j);
+ }
+ return i;
+}
+function equalsZero(i) {
+  if (i==0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isPlural(i) {
+  if (i>1) {
+    return 's';
+  } else {
+    return '';
+  }
+}
+
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
