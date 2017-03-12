fcb78a8591f69791103717241f022a8fec7b9771
Guilherme Bruno <spamguetz@gmail.com>
2017-03-12 19:27:29 -0300

    Cleaning
    Reboot.

--- bait-detector/js/index.js
+++
@@ -1,3 +0,0 @@
-function trigger() {
-  $('.centered').empty().append('<h1>Sim, é uma bait.</h1>');
-}
--- grecu/js/index.js
+++
@@ -1,8 +0,0 @@
-function count() {
-  var date = new Date;
-  var unix = date.getTime();
-  var ms = unix - 1422305657000;
-  $('.count').empty().append(ms);
-}
-
-setInterval(function(){count()},50);
